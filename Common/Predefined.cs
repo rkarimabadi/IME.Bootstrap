a1af62f4d4e0c4698d34f5d1f92de9ae4e9204d4
rkarimabadi <rkarimabadi@gmail.com>
2023-01-29 19:25:35 +0330

    Button Component

---
+++ Common/Predefined.cs
@@ -0,0 +1,22 @@
+﻿namespace IME.Bootstrap.Common
+{
+    public enum PredefinedColor
+    {
+        Primary,
+        Secondary,
+        Success,
+        Danger,
+        Warning,
+        Info,
+        Light,
+        Dark
+    }
+    public enum PredefinedSize
+    {
+        sm,
+        md,
+        lg,
+        xl,
+        xxl
+    }
+}
